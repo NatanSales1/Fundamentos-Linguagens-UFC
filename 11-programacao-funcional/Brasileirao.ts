d0dd445eceea1d063c4cf6924d99a6b4a2489734
Natan Sales <72109155+NatanSales1@users.noreply.github.com>
2025-07-28 09:50:36 -0300

    Update Brasileirao.ts

--- 11-programacao-funcional/Brasileirao.ts
+++ 11-programacao-funcional/Brasileirao.ts
@@ -1,1 +1,13 @@
+const resultadosDoTime: ResultadoPartida[] = [
+  { golsMarcados: 2, golsSofridos: 1 }, 
+  { golsMarcados: 1, golsSofridos: 0 }, 
+  { golsMarcados: 0, golsSofridos: 3 }, 
+  { golsMarcados: 3, golsSofridos: 0 }, 
+  { golsMarcados: 2, golsSofridos: 2 }, 
+];
 
+const pontuacaoFinal = calcularPontosTotais(resultadosDoTime, calcularPontosPartida);
+
+console.log(`A pontuação final do time é: ${pontuacaoFinal} pontos.`);
+
+// Saída: A pontuação final do time é: 10 pontos.
